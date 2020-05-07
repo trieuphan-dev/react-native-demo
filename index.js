908c2b1073a5685b163e60782bc3473b9a402187
Trieu Phan <trieu.phanvan@asnet.com.vn>
2020-05-07 09:55:48 +0700

    Added code skeleton project

--- index.js
+++ index.js
@@ -3,7 +3,13 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App'
 import {name as appName} from './app.json';
+import { Provider } from 'mobx-react'
+import stores from './src/stores'
 
-AppRegistry.registerComponent(appName, () => App);
+const MobX = () => (<Provider {...stores}>
+    <App/>
+</Provider>)
+
+AppRegistry.registerComponent(appName, () => MobX);
---
+++ src/Splash.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { NavigationStackScreenProps } from 'react-navigation-stack'
+import { View } from "react-native";
+import SplashStore from './stores/SplashStore'
+import { observer, inject } from 'mobx-react'
+
+import {
+    StyleSheet,
+    Text,
+    ImageBackground
+} from 'react-native';
+
+
+export interface Props extends NavigationStackScreenProps {
+    splashStore: SplashStore
+  }
+
+@inject("splashStore")
+@observer
+export default class Splash extends React.Component<Props> {
+    constructor(props: Props) {
+      super(props)
+    }
+
+    async componentDidMount() {
+        
+      }
+    
+      componentDidUpdate() {
+    
+      }
+  
+    static navigationOptions = {
+      header: null
+    }
+
+    render() {
+      return (
+        <View style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center"
+          }}>
+          <Text>Hello, world Splash!</Text>
+        </View>
+      );
+    }
+  }
