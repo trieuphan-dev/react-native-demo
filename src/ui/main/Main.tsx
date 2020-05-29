4cfb8dd8367cc93854e0f4a49ddb07563863e633
Trieu Phan <trieu.phanvan@asnet.com.vn>
2020-05-29 13:15:33 +0700

    Initial the drawer navigation

--- src/ui/main/Main.tsx
+++ src/ui/main/Main.tsx
@@ -1,11 +1,64 @@
 import * as React from 'react';
 import { observer, inject } from "mobx-react"
-import { View, StyleSheet, ScrollView, RefreshControl, Text, SafeAreaView, TouchableHighlight, Alert, Image } from 'react-native'
+import { View, StyleSheet,Button, ScrollView, RefreshControl, Text, SafeAreaView, TouchableHighlight, Alert, Image } from 'react-native'
 import { NavigationStackScreenProps } from 'react-navigation-stack'
 import ImagesFactory from '../../resources/images/ImagesFactory'
 import AppColor from '../../resources/colors/AppColor'
 import MainStore from '../../stores/main/MainStore';
 import ProgressCircle from 'react-native-progress-circle'
+import { NavigationContainer } from '@react-navigation/native';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
+
+function Feed({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Feed Screen</Text>
+      <Button title="Open drawer" onPress={() => navigation.openDrawer()} />
+      <Button title="Toggle drawer" onPress={() => navigation.toggleDrawer()} />
+    </View>
+  );
+}
+
+function Notifications() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Notifications Screen</Text>
+    </View>
+  );
+}
+
+
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem
+        label="Close drawer"
+        onPress={() => props.navigation.closeDrawer()}
+      />
+      <DrawerItem
+        label="Toggle drawer"
+        onPress={() => props.navigation.toggleDrawer()}
+      />
+    </DrawerContentScrollView>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator drawerContent={props => <CustomDrawerContent {...props} />}>
+      <Drawer.Screen name="Feed" component={Feed} />
+      <Drawer.Screen name="Notifications" component={Notifications} />
+    </Drawer.Navigator>
+  );
+}
 
 export interface Props extends NavigationStackScreenProps {
     mainStore: MainStore
@@ -25,7 +78,9 @@ export interface Props extends NavigationStackScreenProps {
     }
 
     render() {
-        return (<Text>Main Screen</Text>)
+        return (<NavigationContainer>
+          <MyDrawer />
+        </NavigationContainer>)
     }
   }
 
--- src/ui/navigations/AppNavigation.tsx
+++ src/ui/navigations/AppNavigation.tsx
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
-import DrawerNavigator from './DrawerNavigator';
-
 export default createAppContainer(
   createSwitchNavigator({
     // Additional routes such as a login route could
     // be added here:
     // Login: LoginNavigator,
-    Main: DrawerNavigator
   })
 );
